5aa212a3f9fff66f47d342cb8134b3563bf04c6b
andrezho <azhou1@scholastic.com>
2023-10-11 16:14:40 -0400

    Comma was missing

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -6,29 +6,27 @@ function fn() {
 	}
 	var config = {
 		env: env,
-		 BOOKFAIRS_JARVIS_URL: 'https://bookfairs-jarvis.qa.apps.scholastic.tech',
-		 BOOKFAIRS_JARVIS_BASE: 'https://bookfairs-jarvis.qa.apps.scholastic.tech',
-		 BOOKFAIRS_JARVIS_TARGET: 'https://bookfairs-jarvis.qa.apps.scholastic.tech',
-		 BOOKFAIRS_CONTENT_URL: 'https://bookfairs-content.qa.apps.scholastic.tech',
-		 BOOKFAIRS_CONTENT_BASE: 'https://bookfairs-content.qa.apps.scholastic.tech',
-		 BOOKFAIRS_CONTENT_TARGET: 'https://bookfairs-content.qa.apps.scholastic.tech',
-		 EXTERNAL_SCH_COOKIE_URL : 'https://login-qa.scholastic.com',
-		 EXTERNAL_SCH_COOKIE_BASE : 'https://login-qa.scholastic.com',
-		 EXTERNAL_SCH_COOKIE_TARGET : 'https://login-qa.scholastic.com',
-		 CMDM_QA_URL : 'https://qa-internal.api.scholastic.com',
-		 CONTENT_ACCESS_TOKEN : '3dIx0ZzA49dKFMQmZKEPnz3aUWesIafl',
-		 BOOKFAIRS_JARVIS_URL: 'https://bookfairs-jarvis.qa.apps.scholastic.tech/bookfairs-jarvis',
-		 SCHL_LOGIN_URL : 'https://login-qa.scholastic.com/api/login',
-		 CMDM_URL : 'https://qa-internal.api.scholastic.com/cmdm/fair-service/v1',
-		 CMDM_BEARER_TOKEN : '3dIx0ZzA49dKFMQmZKEPnz3aUWesIafl'
-		 BOOKFAIRS_FATPIPE_REPORTS_URL : 'https://fatpipe-reports-api.qa.apps.scholastic.tech',
-         BOOKFAIRS_FATPIPE_REPORTS_BASE : 'https://fatpipe-reports-api.qa.apps.scholastic.tech',
-         BOOKFAIRS_FATPIPE_REPORTS_TARGET : 'https://fatpipe-reports-api.qa.apps.scholastic.tech',
+		BOOKFAIRS_JARVIS_BASE: 'https://bookfairs-jarvis.qa.apps.scholastic.tech',
+		BOOKFAIRS_JARVIS_TARGET: 'https://bookfairs-jarvis.qa.apps.scholastic.tech',
+		BOOKFAIRS_CONTENT_URL: 'https://bookfairs-content.qa.apps.scholastic.tech',
+		BOOKFAIRS_CONTENT_BASE: 'https://bookfairs-content.qa.apps.scholastic.tech',
+		BOOKFAIRS_CONTENT_TARGET: 'https://bookfairs-content.qa.apps.scholastic.tech',
+        EXTERNAL_SCH_COOKIE_URL : 'https://login-qa.scholastic.com',
+        EXTERNAL_SCH_COOKIE_BASE : 'https://login-qa.scholastic.com',
+        EXTERNAL_SCH_COOKIE_TARGET : 'https://login-qa.scholastic.com',
+        CMDM_QA_URL : 'https://qa-internal.api.scholastic.com',
+        CONTENT_ACCESS_TOKEN : '3dIx0ZzA49dKFMQmZKEPnz3aUWesIafl',
+        BOOKFAIRS_JARVIS_URL: 'https://bookfairs-jarvis.qa.apps.scholastic.tech/bookfairs-jarvis',
+        SCHL_LOGIN_URL : 'https://login-qa.scholastic.com/api/login',
+        CMDM_URL : 'https://qa-internal.api.scholastic.com/cmdm/fair-service/v1',
+        CMDM_BEARER_TOKEN : '3dIx0ZzA49dKFMQmZKEPnz3aUWesIafl',
+        BOOKFAIRS_FATPIPE_REPORTS_URL : 'https://fatpipe-reports-api.qa.apps.scholastic.tech',
+        BOOKFAIRS_FATPIPE_REPORTS_BASE : 'https://fatpipe-reports-api.qa.apps.scholastic.tech',
+        BOOKFAIRS_FATPIPE_REPORTS_TARGET : 'https://fatpipe-reports-api.qa.apps.scholastic.tech',
 
 	}
-	
 	if (env == 'qa') {
-		
+
 		config.BOOKFAIRS_JARVIS_URL = 'https://bookfairs-jarvis.qa.apps.scholastic.tech';
 		config.BOOKFAIRS_JARVIS_BASE= 'https://bookfairs-jarvis.qa.apps.scholastic.tech';
 		config.BOOKFAIRS_JARVIS_TARGET = 'https://bookfairs-jarvis.qa.apps.scholastic.tech';
@@ -36,16 +34,16 @@ function fn() {
         config.BOOKFAIRS_CONTENT_BASE = 'https://bookfairs-content.qa.apps.scholastic.tech';
         config.BOOKFAIRS_CONTENT_TARGET = 'https://bookfairs-content.qa.apps.scholastic.tech';
 		config.EXTERNAL_SCH_COOKIE_URL = 'https://login-qa.scholastic.com';
-		 config.EXTERNAL_SCH_COOKIE_BASE = 'https://login-qa.scholastic.com';
-		 config.EXTERNAL_SCH_COOKIE_TARGET = 'https://login-qa.scholastic.com';
-		 config.CMDM_QA_URL = 'https://qa-internal.api.scholastic.com';
-		 config.CONTENT_ACCESS_TOKEN = '3dIx0ZzA49dKFMQmZKEPnz3aUWesIafl';
-		 config.BOOKFAIRS_FATPIPE_REPORTS_URL = 'https://fatpipe-reports-api.qa.apps.scholastic.tech';
-		 config.BOOKFAIRS_FATPIPE_REPORTS_BASE = 'https://fatpipe-reports-api.qa.apps.scholastic.tech';
-		 config.BOOKFAIRS_FATPIPE_REPORTS_TARGET = 'https://fatpipe-reports-api.qa.apps.scholastic.tech';
+        config.EXTERNAL_SCH_COOKIE_BASE = 'https://login-qa.scholastic.com';
+        config.EXTERNAL_SCH_COOKIE_TARGET = 'https://login-qa.scholastic.com';
+        config.CMDM_QA_URL = 'https://qa-internal.api.scholastic.com';
+        config.CONTENT_ACCESS_TOKEN = '3dIx0ZzA49dKFMQmZKEPnz3aUWesIafl';
+        config.BOOKFAIRS_FATPIPE_REPORTS_URL = 'https://fatpipe-reports-api.qa.apps.scholastic.tech';
+        config.BOOKFAIRS_FATPIPE_REPORTS_BASE = 'https://fatpipe-reports-api.qa.apps.scholastic.tech';
+        config.BOOKFAIRS_FATPIPE_REPORTS_TARGET = 'https://fatpipe-reports-api.qa.apps.scholastic.tech';
 
 	} else if (env == 'dev') {
-		
+
 		config.BOOKFAIRS_JARVIS_URL = 'author';
 		config.BOOKFAIRS_JARVIS_TARGET = 'authorpassword';
 		config.BOOKFAIRS_JARVIS_BASE= '';
